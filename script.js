527feff0a6d849dd2f9e55a2a58467e638e6b26e
Balramrawat <110815042+Balramrawat@users.noreply.github.com>
2025-04-21 14:58:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
+	let table=document.getElementById("sampleTable")
+
+	let extraRow=table.insertRow(0)
+
+	let cell1=extraRow.insertCell()
+	let cell2=extraRow.insertCell()
+
+	cell1.textContent = "New Cell1";
+    cell2.textContent = "New Cell2";
   
   
 }
